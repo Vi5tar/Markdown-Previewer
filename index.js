c223d5696e8ea22692571d822b4be2ebdd4e9d3b
Travis Swiers <tswiers85@gmail.com>
2017-09-24 17:30:48 -0600

    Layout
    * Layed the page out in the desired fashion
    * Added starting text to the input area

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ""
+      input: "Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n*[Travis Swiers](https://github.com/Vi5tar)*"
     };
     this.handleInput = this.handleInput.bind(this);
   }
@@ -16,9 +16,9 @@ class MyComponent extends React.Component {
     return React.createElement(
       "div",
       null,
-      React.createElement("textarea", { value: this.state.input, onChange: this.handleInput })
+      React.createElement("textarea", { id: "inputText", value: this.state.input, onChange: this.handleInput })
     );
   }
 }
 
-ReactDOM.render(React.createElement(MyComponent, null), document.getElementById("markdownPreview"));
+ReactDOM.render(React.createElement(MyComponent, null), document.getElementById("textBox"));
